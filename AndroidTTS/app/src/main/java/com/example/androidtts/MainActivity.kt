c826e33b3ee6dc061535b85d9f6e07b7b0aa7bea
kimjw2003 <kimjw111003@gmail.com>
2020-12-04 20:15:42 +0900

    STT 삽입

--- AndroidTTS/app/src/main/java/com/example/androidtts/MainActivity.kt
+++ AndroidTTS/app/src/main/java/com/example/androidtts/MainActivity.kt
@@ -1,58 +1,83 @@
 package com.example.androidtts
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private var textToSpeech : TextToSpeech? = null
+    private val REQUEST_CODE_SPEECH_INPUT = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textToSpeech = TextToSpeech(this, object: TextToSpeech.OnInitListener {
+        voiceBtn.setOnClickListener {
+            speak()
+        }
 
-            override fun onInit(status:Int) {
-                if (status == TextToSpeech.SUCCESS)
-                {
-                    Log.d("Logd", "1st")
-                    //사용할 언어를 설정
-                    val result = textToSpeech?.setLanguage(Locale.KOREAN)
-                    //언어 데이터가 없거나 혹은 언어가 지원하지 않으면...
-                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
-                    {
-                        Toast.makeText(this@MainActivity, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show()
-                    }
-                    else
-                    {
-                        Log.d("Logd", "2nd")
-                        tts_Btn.setEnabled(true)
-                        //음성 톤
-                        textToSpeech?.setPitch(0.7f)
-                        //읽는 속도
-                        textToSpeech?.setSpeechRate(0.7f)
-                    }
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                Log.d("Logd", "1st")
+                //사용할 언어를 설정
+                val result = textToSpeech?.setLanguage(Locale.KOREAN)
+                //이상한 언어 씨부리면
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(this@MainActivity, "이 언어는 지원하지 않습니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.d("Logd", "2nd")
+                    tts_Btn.setEnabled(true)
+                    //음성 톤
+                    textToSpeech?.setPitch(0.7f)
+                    //읽는 속도
+                    textToSpeech?.setSpeechRate(0.7f)
                 }
             }
-        })
+        }
+        tts_Btn.setOnClickListener { Speech() }
+    } //onCreate
 
-        tts_Btn.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(v: View) {
-                Speech()
-            }
-        })
+    private fun speak() {  //STT
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak something")
+
+        try{
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+        }catch (e : Exception){
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
     }
-    private fun Speech() {
-        val text = edtSpeech.getText().toString()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            REQUEST_CODE_SPEECH_INPUT -> {
+                if(resultCode == Activity.RESULT_OK && null != data){
+                    //음성데이터를 텍스트뷰에 입력
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    textView.text = result!![0]
+                }
+            }
+        }
+    }
+
+    private fun Speech() {  //TTS
+        val text = textView.getText().toString()
         Log.d("Logd", "3rd")
         textToSpeech?.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)// API 20
     }
